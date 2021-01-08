1742b5fc078343aed8e65f40cd713337a410569d
ndeshmukh80 <55752177+ndeshmukh80@users.noreply.github.com>
2021-01-08 23:16:30 +0530

    Update general.py

--- third_party/bi_att_flow/my/tensorflow/general.py
+++ third_party/bi_att_flow/my/tensorflow/general.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from itertools import izip_longest as zip_longest
+from itertools import zip_longest
 
 import tensorflow as tf
 from functools import reduce
